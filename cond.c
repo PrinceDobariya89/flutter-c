6fabbe5e48d914d0504273db076a1972b8d582af
prince dobariya <104968601+PrinceDobariya89@users.noreply.github.com>
2023-02-10 18:50:57 +0530

    Add files via upload

---
+++ cond.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int age,m;
+
+    printf("\nEnter age = ");
+    scanf("%d",&age);
+
+    m = age>18?printf("you are eligible"):printf("you are not eligible");
+
+    
+}
