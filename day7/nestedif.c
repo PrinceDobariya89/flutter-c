8bc662ac6aa555072ed30e0fc043f10909de2d37
prince dobariya <104968601+PrinceDobariya89@users.noreply.github.com>
2023-02-14 15:15:29 +0530

    Add files via upload

---
+++ day7/nestedif.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void main(){
+    int a,b,c,d,e;
+    printf("Enter 5 brother age = ");
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+
+    if (a>c && a>b && a>c && a>d)
+    {
+        if (a>e)
+        {
+            printf("a is bigger");
+        }else{
+            printf("e is bigger");
+        }
+    }else{
+        if (b>d && b>e)
+        {
+         if (b>c)
+         {
+            printf("b is bigger");
+         }else{
+            printf("c is bigger");
+         }
+        }else{
+            if (c>d)
+            {
+             if (c>e)
+             {
+                printf("c is bigger");
+             }else{
+                printf("e is bigger");
+             }
+            }else{
+                printf("d is bigger");
+            }
+        }
+    }
+}
